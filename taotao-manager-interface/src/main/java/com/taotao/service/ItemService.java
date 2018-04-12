d86eff0f238782d8cfc3381dd805e3eff1eb826a
caishuo.cs <caishuo.cs@alibaba-inc.com>
2018-04-12 20:40:38 +0800

    config dubbo for server

---
+++ taotao-manager-interface/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,12 @@
+package com.taotao.service;
+
+import com.taotao.pojo.TbItem;
+
+/**
+ * @author caishuo.cs
+ * @date 2018/4/12
+ */
+public interface ItemService {
+
+    TbItem getItemById(long itemId);
+}
---
+++ taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,24 @@
+package com.taotao.service.impl;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author caishuo.cs
+ * @date 2018/4/12
+ */
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    TbItemMapper itemMapper;
+
+    @Override
+    public TbItem getItemById(long itemId) {
+        TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
+        return tbItem;
+    }
+}
